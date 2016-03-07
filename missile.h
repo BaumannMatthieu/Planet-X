cb7b00347176299e5e8afc28ca0e1dc6bff30dc5
BaumannMatthieu <baumannmatthieu0@gmail.com>
2016-03-08 00:26:13 +0100

    class missile héritera de entity

---
+++ missile.h
@@ -0,0 +1,8 @@
+#ifndef __MISSILE_H__
+#define __MISSILE_H__
+
+class Missile : public Movable {
+     
+};
+
+#endif
--- ship.cpp
+++ ship.cpp
@@ -5,17 +5,27 @@
 
 Ship::Ship(const Rectangle& box) : Entity(box), life_(100),
 rad_focus_(500), rad_attack_(300), rad_near_(100) {
-	mass_ = 5;
+	mass_ = 20;
     max_velocity_ = 10.0f;
     max_force_ = 15; 
 	velocity_ = Vector2<float>(-0.1f, -0.1f);
 	
     Point seek_pos(512.f, 384.f);   
-	
+/*	
     attacking_displacement_.add_point(seek_pos + Point(200.f, 0.f));	    
     attacking_displacement_.add_point(seek_pos + Point(0.f, 200.f));	    
     attacking_displacement_.add_point(seek_pos + Point(-200.f, 0.f));	    
     attacking_displacement_.add_point(seek_pos + Point(0.f, -200.f));	    
+*/
+    uint8_t n = 10;
+    /*
+    for(uint8_t i = 0; i < n; i++) {
+        attacking_displacement_.add_point(seek_pos + Point(200.f*std::cos(2*3.14f*i/n), 200.f*std::sin(2*3.14f*i/n)));
+    }
+    */
+    for(uint8_t i = 0; i < n; i++) {
+        attacking_displacement_.add_point(seek_pos + Point(200.f*std::cos(2*3.14f*i/n), 100.f*std::sin(2*3.14f*i/n)));
+    }
 
 	StatePtr wandering = std::make_shared<State>([this] (const StatePtr current_state) {
 		/* wandering algorithme */
