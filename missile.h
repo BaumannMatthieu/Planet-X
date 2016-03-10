b7f0f4abe61bc242f4d9921288c5b4b3035cbda8
Matthieu Baumann <matthieu.baumann@ensimag.grenoble-inp.fr>
2016-03-11 00:25:14 +0100

    save

--- missile.h
+++ missile.h
@@ -1,8 +1,18 @@
 #ifndef __MISSILE_H__
 #define __MISSILE_H__
 
-class Missile : public Movable {
-         
+class Missile : public Movable,
+                public Renderable {
+    public:
+        Missile(const float degats, const Uint32 cooldown);
+        virtual ~Missile();
+
+		virtual void draw(SDL_Renderer* renderer) = 0;
+		virtual void update() = 0;
+    
+    protected:
+        float degats_;
+        Uint32 cooldown_;
 };
 
 #endif
