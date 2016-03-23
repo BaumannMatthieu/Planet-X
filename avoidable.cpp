8438509c6eb9e9d9926c7cf0cac0bdbac2112d11
Matthieu Baumann <matthieu.baumann@ensimag.grenoble-inp.fr>
2016-03-23 13:46:37 +0100

    most threatening avoidable objects are theses which have the bigger radius

--- avoidable.cpp
+++ avoidable.cpp
@@ -16,7 +16,8 @@ const Circle& Avoidable::get_circle() const {
 }
 
 const AvoidablePtr Avoidable::get_most_threatening_obstacle(const AvoidablePtr& this_entity, const Point& position, const Point& ahead, const Point& ahead2, const std::set<AvoidablePtr>& avoidables) {
-    AvoidablePtr most_threatening = nullptr;    
+    AvoidablePtr most_threatening = nullptr;
+    float bigger_radius_threatening;
 
     for(auto& avoidable : avoidables) {
         if(this_entity != avoidable) {
@@ -28,14 +29,16 @@ const AvoidablePtr Avoidable::get_most_threatening_obstacle(const AvoidablePtr& 
             if(Math::distance(ahead, center) <= radius + this_radius || Math::distance(ahead2, center) <= radius + this_radius) {
                 
                 if(most_threatening != nullptr) {
-                    if(Math::distance(center, position) >= Math::distance(most_threatening->get_circle().get_pos(), position)) {
+                    if(Math::distance(center, position) >= Math::distance(most_threatening->get_circle().get_pos(), position) && radius >= bigger_radius_threatening) {
                         most_threatening = avoidable;
+                        bigger_radius_threatening = radius;
                         continue;
                     }
                 }
 
                 if(most_threatening == nullptr) {
                     most_threatening = avoidable;
+                    bigger_radius_threatening = radius;
                 }
             }
         }
